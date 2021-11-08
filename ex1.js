5d0765921c0f7869ad7d745cb7b0b1a3014261ae
ksahakyan <92794724+ksahakyan@users.noreply.github.com>
2021-11-08 15:35:20 +0400

    HW3
    tasks

---
+++ ex1.js
@@ -0,0 +1,13 @@
+function isPrime(x) {
+    let sqrtNum = Math.floor(Math.sqrt(x)); //x under square root and takes the integer part
+    if ( x <= 1) {
+        return false
+    }
+    for ( let i = 2; i <= sqrtNum; i++ ) {
+        if ( x % i === 0) {
+            return false;
+        }
+    }
+    return true
+}
+console.log(isPrime(6));
